fe9f96b07de74115c6e2790e590df0fc042ed808
unknown <japajao25@gmail.com>
2026-03-20 17:58:22 -0300

    entidade criada

---
+++ GerenciamentoCategoria/src/main/java/com/atividade/GerenciamentoCategoria/GerenciamentoCategoriaApplication.java
@@ -0,0 +1,13 @@
+package com.atividade.GerenciamentoCategoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoCategoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoCategoriaApplication.class, args);
+	}
+
+}
---
+++ GerenciamentoCategoria/src/test/java/com/atividade/GerenciamentoCategoria/GerenciamentoCategoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade.GerenciamentoCategoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoCategoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
